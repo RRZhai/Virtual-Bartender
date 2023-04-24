b1b41860593f127d11eae11168240d3a2226a064
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-04-24 15:58:35 -0700

    drink cards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+// global viable
+const cards = document.getElementById('cards')
+
+
 const fetchDrink = () => {
-    
+    fetch("https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php")
+    .then(resp => resp.json())
+    // .then(drinkData => {debugger})
+    .then(drinkData => drinkData.drinks.forEach(drink => renderDrink(drink)))
+}
+fetchDrink()
+
+const renderDrink = drink => {
+    const drinkDtl = document.createElement('div')
+    drinkDtl.className = 'single-card'
+    cards.appendChild(drinkDtl)
+    const drinkImg = document.createElement('img')
+    drinkImg.className = 'drink-img'
+    drinkDtl.appendChild(drinkImg)
+    const drinkInfo = document.createElement('div')
+    drinkInfo.className = "drink-info"
+    drinkDtl.appendChild(drinkInfo)
+    const drinkName = document.createElement('div')
+    drinkName.className = 'drink-name'
+    drinkInfo.appendChild(drinkName)
+    const drinkType = document.createElement('li')
+    const drinkIng = document.createElement('li')
+    drinkInfo.appendChild(drinkType)
+    drinkInfo.appendChild(drinkIng)
+    drinkImg.src = drink.strDrinkThumb
+    drinkName.textContent = drink.strDrink
+    drinkType.textContent = drink.strCategory
+    drinkIng.textContent = drink.strIngredient1
 }
