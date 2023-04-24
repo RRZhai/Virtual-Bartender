a35149b981e74ecb88c49536f4a929b073d3243e
nolan <n0lan.n4sh@gmail.com>
2023-04-24 15:54:50 -0700

    for some reason the README didnt update

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ const searchForm = document.getElementById('searchForm')
 const searchInput = document.getElementById('searchInput')
 const dropDown = document.getElementById('searchFilter')
 
+
+//search filter will take in an array of drinks from fetch, and sort out any ones that meet the .value of searchFilter
+//order of operations --> list starts empty --> on search activates searchFilter which chooses which fetch function to use, 
 const searchFilter = () =>{
     let selectedFilter = searchFilter.value
-    console.log(selectedFilter) 
+    
 }
-searchFilter()
+
+const fetch4DrinkIngredients = () =>{
+    fetch('')
+    .then()
+    .then()
+}
+
