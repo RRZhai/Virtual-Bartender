1def12b5b248d2d57652b5e59b5798848bb8c4e7
rcblake <127946751+rcblake@users.noreply.github.com>
2023-04-24 16:52:51 -0700

    add initial fetch with the preset 10 drink random

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-const entireSearchNav = document.getElementById('searchNav')
-const searchForm = document.getElementById('searchForm')
-const searchInput = document.getElementById('searchInput')
-const dropDown = document.getElementById('searchFilter')
+const entireSearchNav = document.getElementById("searchNav");
+const searchForm = document.getElementById("searchForm");
+const searchInput = document.getElementById("searchInput");
+const dropDown = document.getElementById("searchFilter");
 
+//Initial Page fetch with 10 random drinks
+fetch("https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php")
+  .then((res) => res.json())
+  .then((randomDrinksArray) =>
+    randomDrinksArray.forEach((oneRandomDrink) =>
+      renderDrinkCard(oneRandomDrink)
+    )
+  );
 
 //search filter will take in an array of drinks from fetch, and sort out any ones that meet the .value of searchFilter
-//order of operations --> list starts empty --> on search activates searchFilter which chooses which fetch function to use, 
-const searchFilter = () =>{
-    let selectedFilter = searchFilter.value
-    
-}
+//order of operations --> list starts empty --> on search activates searchFilter which chooses which fetch function to use,
+const searchFilter = () => {
+  let selectedFilter = searchFilter.value;
+};
 
-const fetch4DrinkIngredients = () =>{
-    fetch('')
-    .then()
-    .then()
-}
-
-
+const fetch4DrinkIngredients = () => {
+  fetch("").then().then();
+};
