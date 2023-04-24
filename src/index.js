3cfdd9a338f7da7ea2bcf766dae6ad9a113fb29e
nolan <n0lan.n4sh@gmail.com>
2023-04-24 15:01:30 -0700

    all of my current work but actually saved and up to date

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const entireSearchNav = document.getElementById('searchNav')
+const searchForm = document.getElementById('searchForm')
+const searchInput = document.getElementById('searchInput')
+const dropDown = document.getElementById('searcgFilter')
