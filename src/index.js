1300abfa5632c7454edf7643e10fe8b1797e11bf
nolan <n0lan.n4sh@gmail.com>
2023-04-24 15:14:38 -0700

    constants and start of searchFilter function in js --> smidge more formatting css/html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const entireSearchNav = document.getElementById('searchNav')
 const searchForm = document.getElementById('searchForm')
 const searchInput = document.getElementById('searchInput')
-const dropDown = document.getElementById('searcgFilter')
+const dropDown = document.getElementById('searchFilter')
+
+const searchFilter = () =>{
+    let selectedFilter = searchFilter.value
+    console.log(selectedFilter) 
+}
+searchFilter()
