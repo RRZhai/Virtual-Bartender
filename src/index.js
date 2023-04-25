365262e558b60452d1b5f6b45122563dbdea2b85
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-04-24 21:02:39 -0700

    add click listener for the card

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 // global viable
-const cards = document.getElementById('cards')
+const container = document.getElementById('container')
 
 
 const fetchDrink = () => {
     fetch("https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php")
     .then(resp => resp.json())
     // .then(drinkData => {debugger})
-    .then(drinkData => drinkData.drinks.forEach(drink => renderDrink(drink)))
+    .then(drinkData => {
+        drinkData.drinks.forEach(drink => {
+            renderDrink(drink)
+            // ingArr(drink)
+        })
+    })
 }
 fetchDrink()
 
 const renderDrink = drink => {
+    const cards = document.createElement('div')
+    cards.className = 'cards'
+    container.appendChild(cards)
     const drinkDtl = document.createElement('div')
     drinkDtl.className = 'single-card'
     cards.appendChild(drinkDtl)
@@ -31,4 +39,24 @@ const renderDrink = drink => {
     drinkName.textContent = drink.strDrink
     drinkType.textContent = drink.strCategory
     drinkIng.textContent = drink.strIngredient1
+    cards.addEventListener('click', (e) => {
+        const drinkIngredient = document.createElement('div')
+        drinkIngredient.className = "ingredient"
+        const drinkReceipe = document.createElement('p')
+        drinkReceipe.className = 'receipe'
+        drinkInfo.after(drinkIngredient)
+        drinkInfo.after(drinkReceipe)
+        drinkReceipe.innerText = drink.strInstructions
+        drinkIngredient.innerText = `${drink.strMeasure1} ${drink.strIngredient1}, 
+        ${drink.strMeasure2} ${drink.strIngredient2}, 
+        ${drink.strMeasure3} ${drink.strIngredient3}, 
+        ${drink.strMeasure4} ${drink.strIngredient4}, 
+        ${drink.strMeasure5} ${drink.strIngredient4}` 
+    })
 }
+
+// const IngArr = (drink) => {
+//     drink.filter(() => {
+
+//     })
+// }
