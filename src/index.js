82f46c44f147364feea9d85e94ed912faf8198f9
nolan <n0lan.n4sh@gmail.com>
2023-04-25 13:17:36 -0700

    currently search is working for the name filter, will work on ingredient filter after lunch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+// LINES WITH ** in comment are lines that seem like we can delete(?)
 //////global constants/////
 
 //search Bar/Nav constants
 const entireSearchNav = document.querySelector('#searchNav')
-const formSearch = document.querySelector('#searchInput')
+const formSearchInput = document.querySelector('#searchInput')
 const searchFilterDD = document.querySelector('#searchFilter')
 const ingrFilter = document.querySelector('#ingrFilter')
 const nameFilter = document.querySelector('#nameFilter')
+const searchForm = document.querySelector('#searchForm')
 
 //Side panel constants
 const sidePanelContainer = document.getElementById("filter-section");
@@ -15,8 +17,26 @@ const spiritFtrLst = document.getElementById("spirit-filter-list");
 const drinkTypeFtrLst = document.getElementById("type-filter-list");
 const spiritExpandBtn = document.getElementById("spirit-expand-btn");
 
-
 const container = document.getElementById('container')
+
+//functions
+
+const userSearchByName = () => {
+    input = encodeURI(formSearchInput.value)
+    const searchNameUrl = 'https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=' + input
+    console.log(searchNameUrl)
+    fetch(searchNameUrl)
+    .then(res => res.json())
+    .then(searchedArray => {searchedArray.drinks.forEach(drink => renderDrink(drink))})
+}
+const userSearchByIngredient = (e) => {
+    input = encodeURI(e.target['searchInput'].value)
+    const searchNameUrl = 'https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=' + input
+    console.log(searchNameUrl)
+    fetch(searchNameUrl)
+    .then(res => res.json())
+    .then(drinksArray => drinksArray.drinks.forEach(drink => renderDrink(drink)))
+}
 
 
 const fetchDrink = () => {
@@ -30,6 +50,7 @@ const fetchDrink = () => {
         })
     })
 }
+
 fetchDrink()
 
 const renderDrink = drink => {
@@ -72,6 +93,7 @@ const renderDrink = drink => {
     })
 }
 
+//** 
 // const IngArr = (drink) => {
 //     drink.filter(() => {
 
@@ -91,30 +113,13 @@ function retchRandom() {
     );
 }
 
-function renderOneDrinkCard(drinkObj) {}
+//** 
+//function renderOneDrinkCard(drinkObj) {}
 
 //helper functions
-const handleSubmit = (e) =>{
-    //always inlcude p.dflt
-    e.preventDefault()
-    //user search as var.
-    // https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mar -> this works
-    //why do you hate me Mr. URL
-    const userSearch = encodeURI(e.target['searchInput'].value)
-    if (searchFilterDD.textContent === 'Drink Name'){
-        const searchNameURL = 'https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=' + userSearch
-        fetch(searchNameURL)
-        .then(res => res.json())
-        .then(drinksArray => drinksArray.forEach(drink=> {soughtDrinksClicker(drink)}))}
-    else if(searchFilterDD.textContent === 'Ingredient Filter'){
-        const searchNameURL = 'https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=' + userSearch
-        fetch(searchNameURL)
-        .then(res => res.json())
-        .then(drinksArray => drinksArray.forEach(drink=> {soughtDrinksClicker(drink)}))}
-}
 
-//search filter will take in an array of drinks from fetch, the fetch should be already sorted --> just need to append them and make em clickable
-//order of operations --> list starts empty --> on search activates searchFilter which chooses which fetch function to use, 
+
+
 
 // spiritExpandBtn.addEventListener(
 //   "click",
@@ -125,4 +130,7 @@ spiritExpandBtn.appendChild(spiritFtrLst);
 
 
 ///event listeners:
-document.addEventListener('submit', handleSubmit)
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    userSearchByName()
+})
