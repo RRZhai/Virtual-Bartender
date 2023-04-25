68cd47f02251729b0fad33071cc3e5ec4935370a
rcblake <127946751+rcblake@users.noreply.github.com>
2023-04-25 16:08:07 -0700

    safety commit before sync

--- src/index.js
+++ src/index.js
@@ -1,60 +1,78 @@
-
-// global viable
-const container = document.getElementById('container')
-
+// global variable
+const container = document.getElementById("container");
 
 const fetchDrink = () => {
-    fetch("https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php")
-    .then(resp => resp.json())
+  fetch("https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php")
+    .then((resp) => resp.json())
     // .then(drinkData => {debugger})
-    .then(drinkData => {
-        drinkData.drinks.forEach(drink => {
-            renderDrink(drink)
-            // ingArr(drink)
-        })
-    })
-}
-fetchDrink()
+    .then((drinkData) => {
+      drinkData.drinks.forEach((drink) => {
+        renderDrink(drink);
+        // ingArr(drink)
+      });
+    });
+};
+fetchDrink();
 
-const renderDrink = drink => {
-    const cards = document.createElement('div')
-    cards.className = 'cards'
-    container.appendChild(cards)
-    const drinkDtl = document.createElement('div')
-    drinkDtl.className = 'single-card'
-    cards.appendChild(drinkDtl)
-    const drinkImg = document.createElement('img')
-    drinkImg.className = 'drink-img'
-    drinkDtl.appendChild(drinkImg)
-    const drinkInfo = document.createElement('div')
-    drinkInfo.className = "drink-info"
-    drinkDtl.appendChild(drinkInfo)
-    const drinkName = document.createElement('div')
-    drinkName.className = 'drink-name'
-    drinkInfo.appendChild(drinkName)
-    const drinkType = document.createElement('li')
-    const drinkIng = document.createElement('li')
-    drinkInfo.appendChild(drinkType)
-    drinkInfo.appendChild(drinkIng)
-    drinkImg.src = drink.strDrinkThumb
-    drinkName.textContent = drink.strDrink
-    drinkType.textContent = drink.strCategory
-    drinkIng.textContent = drink.strIngredient1
-    cards.addEventListener('click', (e) => {
-        const drinkIngredient = document.createElement('div')
-        drinkIngredient.className = "ingredient"
-        const drinkReceipe = document.createElement('p')
-        drinkReceipe.className = 'receipe'
-        drinkInfo.after(drinkIngredient)
-        drinkInfo.after(drinkReceipe)
-        drinkReceipe.innerText = drink.strInstructions
-        drinkIngredient.innerText = `${drink.strMeasure1} ${drink.strIngredient1}, 
+const presetFilterBtns = document.querySelector(".preset-filter-btn");
+presetFilterBtns.addEventListener("click", (e) => fetchPresets(e));
+
+const fetchPresets = (e) => {
+  debugger;
+  const presetName = e.target.value;
+  fetch(`https://www.thecocktaildb.com/api/json/v2/9973533/${presetName}.php`)
+    .then((res) => res.json())
+    .then((drinkData) =>
+      drinkData.drinks.forEach((drink) => renderDrink(drink))
+    );
+};
+
+const renderDrink = (drink) => {
+  const cards = document.createElement("div");
+  cards.className = "cards";
+  container.appendChild(cards);
+  //We can remove the above because we'll append to the existing HTML section
+  const drinkDtl = document.createElement("div");
+  drinkDtl.className = "single-card";
+  cards.appendChild(drinkDtl);
+  //Can we change this variable to be about a single card like drinkCard?
+  const drinkImg = document.createElement("img");
+  drinkImg.className = "drink-img";
+  drinkDtl.appendChild(drinkImg);
+  const drinkInfo = document.createElement("div");
+  drinkInfo.className = "drink-info";
+  drinkDtl.appendChild(drinkInfo);
+  const drinkName = document.createElement("div");
+  drinkName.className = "drink-name";
+  drinkInfo.appendChild(drinkName);
+  //let's make this a header within the card instead of it's own div
+  const drinkType = document.createElement("li");
+  const drinkIng = document.createElement("li");
+  drinkInfo.appendChild(drinkType);
+  drinkInfo.appendChild(drinkIng);
+  //these can be p's which just have less default formatting
+  drinkImg.src = drink.strDrinkThumb;
+  drinkName.textContent = drink.strDrink;
+  drinkType.textContent = drink.strCategory;
+  drinkIng.textContent = drink.strIngredient1;
+  //
+
+  // Let's separate these fucntions and use "drink" as the argument
+  cards.addEventListener("click", (e) => {
+    const drinkIngredient = document.createElement("div");
+    drinkIngredient.className = "ingredient";
+    const drinkReceipe = document.createElement("p");
+    drinkReceipe.className = "receipe";
+    drinkInfo.after(drinkIngredient);
+    drinkInfo.after(drinkReceipe);
+    drinkReceipe.innerText = drink.strInstructions;
+    drinkIngredient.innerText = `${drink.strMeasure1} ${drink.strIngredient1}, 
         ${drink.strMeasure2} ${drink.strIngredient2}, 
         ${drink.strMeasure3} ${drink.strIngredient3}, 
         ${drink.strMeasure4} ${drink.strIngredient4}, 
-        ${drink.strMeasure5} ${drink.strIngredient4}` 
-    })
-}
+        ${drink.strMeasure5} ${drink.strIngredient4}`;
+  });
+};
 
 // const IngArr = (drink) => {
 //     drink.filter(() => {
@@ -75,19 +93,6 @@ const spiritFtrLst = document.getElementById("spirit-filter-list");
 const drinkTypeFtrLst = document.getElementById("type-filter-list");
 const spiritExpandBtn = document.getElementById("spirit-expand-btn");
 
-//Initial Page fetch with 10 random drinks for
-function retchRandom() {
-  fetch("https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php")
-    .then((res) => res.json())
-    .then((randomDrinksArray) =>
-      randomDrinksArray.forEach((oneRandomDrink) =>
-        renderOneDrinkCard(oneRandomDrink)
-      )
-    );
-}
-
-function renderOneDrinkCard(drinkObj) {}
-
 //search filter will take in an array of drinks from fetch, and sort out any ones that meet the .value of searchFilter
 //order of operations --> list starts empty --> on search activates searchFilter which chooses which fetch function to use,
 const searchFilter = () => {
@@ -98,10 +103,17 @@ const fetch4DrinkIngredients = () => {
   fetch("").then().then();
 };
 
-// spiritExpandBtn.addEventListener(
-//   "click",
-//   e.target.child.classList.toggle("hidden")
-// );
+//create an array of the first couple ingredients for each card, this will be rendered with card
+const ingStr1 = drink.strIngredient1.toLowerCase();
+const ingStr2 = drink.strIngredient2.toLowerCase();
+drinkCard.classList.add(ingStr1(ingStr2));
 
-spiritExpandBtn.appendChild(spiritFtrLst);
+const allFilterOptions = document.getElementsByClassName("filter-input");
+allFilterOptions.addEventListener("change", e, handleFilter(e));
 
+function handleFilter(returnArray) {
+  //can I get this to find if the value is contained in ingredients
+  if (drinkCard.CalssList.contains(e.target.value)) {
+    return true;
+  }
+}
