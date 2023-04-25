6340be4f889984466708557832da3e13333695c0
nolan <n0lan.n4sh@gmail.com>
2023-04-25 10:16:35 -0700

    fixed fetch requests --> next step adding to search result div

--- src/index.js
+++ src/index.js
@@ -3,30 +3,30 @@ const entireSearchNav = document.querySelector('#searchNav')
 const formSearch = document.querySelector('#searchInput')
 const searchFilterDD = document.querySelector('#searchFilter')
 const ingrFilter = document.querySelector('#ingrFilter')
-const nameFilter = document.querySelector('#categoryFilter')
+const nameFilter = document.querySelector('#nameFilter')
 
 
 //helper functions
 const handleSubmit = (e) =>{
     //always inlcude p.dflt
     e.preventDefault()
-    
     //user search as var.
+    // https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mar -> this works
+    //why do you hate me Mr. URL
     const userSearch = encodeURI(e.target['searchInput'].value)
-    debugger
-    //if (searchFilterDD.textContent === 'Drink Name')
-        fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s=${userSearch}`)
+    if (searchFilterDD.textContent === 'Drink Name'){
+        const searchNameURL = 'https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=' + userSearch
+        fetch(searchNameURL)
         .then(res => res.json())
-        .then(drinksArray => console.log(drinksArray))
-        debugger
-    
-   
+        .then(drinksArray => drinksArray.forEach(drink=> {soughtDrinksClicker(drink)}))}
+    else if(searchFilterDD.textContent === 'Ingredient Filter'){
+        const searchNameURL = 'https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=' + userSearch
+        fetch(searchNameURL)
+        .then(res => res.json())
+        .then(drinksArray => drinksArray.forEach(drink=> {soughtDrinksClicker(drink)}))}
 }
 
-
-
-
-//search filter will take in an array of drinks from fetch, and sort out any ones that meet the .value of searchFilter
+//search filter will take in an array of drinks from fetch, the fetch should be already sorted --> just need to append them and make em clickable
 //order of operations --> list starts empty --> on search activates searchFilter which chooses which fetch function to use, 
 
 
