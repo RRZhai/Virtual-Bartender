b46b4fa3e45af8355db80b85ee60718086746a06
nolan <n0lan.n4sh@gmail.com>
2023-04-25 08:59:36 -0700

    decided to move away from auto complete search bar for now, struggling with fetch request...

--- src/index.js
+++ src/index.js
@@ -1,34 +1,35 @@
-const entireSearchNav = document.getElementById('searchNav')
-const formSearch = document.getElementById('searchForm')
-const inputSearch = document.getElementById('searchInput')
-const dropDownSearch = document.getElementById('searchFilter')
+//global constants
+const entireSearchNav = document.querySelector('#searchNav')
+const formSearch = document.querySelector('#searchInput')
+const searchFilterDD = document.querySelector('#searchFilter')
+const ingrFilter = document.querySelector('#ingrFilter')
+const nameFilter = document.querySelector('#categoryFilter')
 
-let mainIngredientsSuggestions = [];
-let drinkCategorySuggestions = [];
 
-const fetchSuggestions = (listSpecifier,pushedArray) =>{
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?`+ `${listSpecifier}` + `=list`)
-    .then(res => res.json())
-    .then(drinks => drinks.forEach(drink =>{ pushedArray.push(drink.strCategory)}))
-    console.log(drink)
+//helper functions
+const handleSubmit = (e) =>{
+    //always inlcude p.dflt
+    e.preventDefault()
+    
+    //user search as var.
+    const userSearch = encodeURI(e.target['searchInput'].value)
+    debugger
+    //if (searchFilterDD.textContent === 'Drink Name')
+        fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s=${userSearch}`)
+        .then(res => res.json())
+        .then(drinksArray => console.log(drinksArray))
+        debugger
+    
+   
 }
 
-const mainIngredients = fetchSuggestions('i',mainIngredientsSuggestions)
-const drinkCategories = fetchSuggestions('c',drinkCategorySuggestions)
-console.log(mainIngredients)
+
 
 
 //search filter will take in an array of drinks from fetch, and sort out any ones that meet the .value of searchFilter
 //order of operations --> list starts empty --> on search activates searchFilter which chooses which fetch function to use, 
-const searchFilter = () =>{
-    let selectedFilter = searchFilter.value
-    
-}
-
-const fetch4DrinkIngredients = () =>{
-    fetch('')
-    .then()
-    .then()
-}
 
 
+
+///event listeners:
+document.addEventListener('submit', handleSubmit)
