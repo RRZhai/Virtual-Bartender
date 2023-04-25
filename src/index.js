bc3252a337bdac321e7a2edc9d400f5677f12159
nolan <n0lan.n4sh@gmail.com>
2023-04-24 17:47:59 -0700

    decided to keep all the work in the same file for now, encountering issues with trying to make this into one condensed function...

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ const formSearch = document.getElementById('searchForm')
 const inputSearch = document.getElementById('searchInput')
 const dropDownSearch = document.getElementById('searchFilter')
 
+let mainIngredientsSuggestions = [];
+let drinkCategorySuggestions = [];
+
+const fetchSuggestions = (listSpecifier,pushedArray) =>{
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?`+ `${listSpecifier}` + `=list`)
+    .then(res => res.json())
+    .then(drinks => drinks.forEach(drink =>{ pushedArray.push(drink.strCategory)}))
+    console.log(drink)
+}
+
+const mainIngredients = fetchSuggestions('i',mainIngredientsSuggestions)
+const drinkCategories = fetchSuggestions('c',drinkCategorySuggestions)
+console.log(mainIngredients)
+
 
 //search filter will take in an array of drinks from fetch, and sort out any ones that meet the .value of searchFilter
 //order of operations --> list starts empty --> on search activates searchFilter which chooses which fetch function to use, 
