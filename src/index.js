f44a14e12caa0d2d72b99ba553ee8f63656744bd
rcblake <127946751+rcblake@users.noreply.github.com>
2023-04-26 12:43:06 -0700

    fix main

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 // LINES WITH ** in comment are lines that seem like we can delete(?) or were commented out before and need looking at
 //////global constants/////
 
@@ -19,16 +18,16 @@ const searchForm = document.querySelector("#searchForm");
 
 //card constants (shiyao)
 
-const cards = document.querySelector('.cards')
-const modalName = document.getElementById('drink-name-modal')
-const modalImg = document.getElementById('img-modal')
-const ingAndMea = document.getElementById('ingredient-measure')
-const modalInst = document.getElementById('instructions-modal')
+const cards = document.querySelector(".cards");
+const modalName = document.getElementById("drink-name-modal");
+const modalImg = document.getElementById("img-modal");
+const ingAndMea = document.getElementById("ingredient-measure");
+const modalInst = document.getElementById("instructions-modal");
 
 //buttons n stuff constants:
-const surpriseBtn = document.querySelector('#random-filter-btn')
-const popularBtn = document.querySelector('#most-popular-btn')
-const latestDrinksBtn = document.querySelector('#new-recipies-btn')
+// const surpriseBtn = document.querySelector("#random-filter-btn");
+// const popularBtn = document.querySelector("#most-popular-btn");
+// const latestDrinksBtn = document.querySelector("#new-recipies-btn");
 
 //functions
 
@@ -37,6 +36,7 @@ const fetchDrink = () => {
   fetch("https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php")
     .then((resp) => resp.json())
     .then((drinkData) => {
+      console.log(drinkData);
       drinkData.drinks.forEach((drink) => {
         renderDrink(drink);
       });
@@ -48,107 +48,98 @@ fetchDrink();
 //need to figure out why
 
 const userSearchByName = (drinkName) => {
-    const input = (formSearchInput.value)
-    const URLinput = encodeURI(input.replace(' ','_'));
-   // console.log(URLinput)
-   const query = drinkName ? drinkName : URLinput 
-   const searchNameUrl = 'https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=' + query
-    fetch(searchNameUrl)
-    .then(res => res.json())
-    .then(searchedArray => {
-        searchedArray.drinks.forEach(drink => {
-            renderDrink(drink)
-        })
-    })
-}
+  const input = formSearchInput.value;
+  const URLinput = encodeURI(input.replace(" ", "_"));
+  // console.log(URLinput)
+  const query = drinkName ? drinkName : URLinput;
+  const searchNameUrl =
+    "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=" + query;
+  fetch(searchNameUrl)
+    .then((res) => res.json())
+    .then((searchedArray) => {
+      searchedArray.drinks.forEach((drink) => {
+        renderDrink(drink);
+      });
+    });
+};
 
-//is the issue here that because I am using ingredient as a search parameter --> youre just fetching the drink and its id --> that data needs to somehow go into 
+//is the issue here that because I am using ingredient as a search parameter --> youre just fetching the drink and its id --> that data needs to somehow go into
 const userSearchByIngredient = () => {
-    const input = (formSearchInput.value)
-    URLinput = encodeURI(input.replace(' ','_'));
-    const searchNameUrl = 'https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=' + URLinput
-    console.log(URLinput)
-    fetch(searchNameUrl)
-    .then(res => res.json())
-    .then(searchedArray => {
-        const namesArray = searchedArray.drinks.map(drink =>{
-        return drink.strDrink
-        })
-        namesArray.forEach(userSearchByName)
-    })
-}
+  const input = formSearchInput.value;
+  URLinput = encodeURI(input.replace(" ", "_"));
+  const searchNameUrl =
+    "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" +
+    URLinput;
+  console.log(URLinput);
+  fetch(searchNameUrl)
+    .then((res) => res.json())
+    .then((searchedArray) => {
+      const namesArray = searchedArray.drinks.map((drink) => {
+        return drink.strDrink;
+      });
+      namesArray.forEach(userSearchByName);
+    });
+};
 
-const renderDrink = drink => {
-  const card = document.createElement('div')
-  card.className = 'single-card'
-  cards.appendChild(card)
-  const drinkImg = document.createElement('img')
-  drinkImg.className = 'drink-img'
-  drinkImg.src = drink.strDrinkThumb
-  const drinkName = document.createElement('div')
-  drinkName.className = 'drink-info'
-  drinkName.innerText = drink.strDrink
-  card.append(drinkImg, drinkName)
-  drinkInfo(drink, drinkName)
-  drinkImg.addEventListener('click', () => handleDrink(drink))
-}
+const renderDrink = (drink) => {
+  console.log(drink);
+  const card = document.createElement("div");
+  card.className = "single-card";
+  cards.appendChild(card);
+  const drinkImg = document.createElement("img");
+  drinkImg.className = "drink-img";
+  drinkImg.src = drink.strDrinkThumb;
+  const drinkName = document.createElement("div");
+  drinkName.className = "drink-info";
+  drinkName.innerText = drink.strDrink;
+  card.append(drinkImg, drinkName);
+  drinkInfo(drink, drinkName);
+  drinkImg.addEventListener("click", () => handleDrink(drink));
+};
 
 const handleDrink = (drink) => {
-  modalImg.src = drink.strDrinkThumb
+  modalImg.src = drink.strDrinkThumb;
   ingAndMea.innerText = `${ingredientList(drink)}  
-                        ${measureList(drink)}`
-  modalName.innerText = drink.strDrink
-  modalInst.textContent = drink.strInstructions
-}
-
+                        ${measureList(drink)}`;
+  modalName.innerText = drink.strDrink;
+  modalInst.textContent = drink.strInstructions;
+};
 
 const ingredientList = (drink) => {
-    let ingreKeyArr = Object.keys(drink).filter(keys => {
-        return keys[12] === 't'
-    })
-    const ingreArr = []
-    for (let i = 0; i < ingreKeyArr.length; i++ ){
-      if (drink[ingreKeyArr[i]]){
-        ingreArr.push(drink[ingreKeyArr[i]])
-      }
-    }
-    return ingreArr
-}
-
-
-const measureList = (drink) => {
-  let meaKeyArr = Object.keys(drink).filter(keys => {
-      return keys[9] === 'e'
-  })
-  const meaArr = []
-  for (let i = 0; i < meaKeyArr.length; i++ ){
-    if (drink[meaKeyArr[i]]){
-      meaArr.push(drink[meaKeyArr[i]])
+  let ingreKeyArr = Object.keys(drink).filter((keys) => {
+    return keys[12] === "t";
+  });
+  const ingreArr = [];
+  for (let i = 0; i < ingreKeyArr.length; i++) {
+    if (drink[ingreKeyArr[i]]) {
+      ingreArr.push(drink[ingreKeyArr[i]]);
     }
   }
-  return meaArr
-}
+  return ingreArr;
+};
+
+const measureList = (drink) => {
+  let meaKeyArr = Object.keys(drink).filter((keys) => {
+    return keys[9] === "e";
+  });
+  const meaArr = [];
+  for (let i = 0; i < meaKeyArr.length; i++) {
+    if (drink[meaKeyArr[i]]) {
+      meaArr.push(drink[meaKeyArr[i]]);
+    }
+  }
+  return meaArr;
+};
 
 const drinkInfo = (drink, drinkName) => {
-  const drinkType = document.createElement('li')
-  drinkType.textContent = drink.strAlcoholic
-  const drinkIng1 = document.createElement('li')
-  drinkIng1.textContent = drink.strIngredient1
-  drinkName.append(drinkType, drinkIng1)
-}
-
-
-
+  const drinkType = document.createElement("li");
+  drinkType.textContent = drink.strAlcoholic;
+  const drinkIng1 = document.createElement("li");
+  drinkIng1.textContent = drink.strIngredient1;
+  drinkName.append(drinkType, drinkIng1);
+};
 
 // S end
-
-//Initial Page fetch with 10 random drinks for
-function fetchRandom() {
-  fetch("https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php")
-    .then(res => res.json())
-    .then(randomDrinksArray => randomDrinksArray.drinks.forEach(oneRandomDrink =>renderDrink(oneRandomDrink)));
-}
-
 
 //search filter will take in an array of drinks from fetch, and sort out any ones that meet the .value of searchFilter
 //order of operations --> list starts empty --> on search activates searchFilter which chooses which fetch function to use,
@@ -181,6 +172,7 @@ document.addEventListener("click", (e) => {
       .then((res) => res.json())
       .then((drinkData) => {
         container.innerHTML = "";
+        console.log(drinkData);
         drinkData.drinks.forEach((drink) => {
           renderDrink(drink);
         });
@@ -196,5 +188,3 @@ document.addEventListener("change", (e) => {
   const isCheckBoxClick = e.target.classList.contains("filter-input"); // adding event listener to eachcheckbox (with matching class)
   console.log("checkbox got clicked"); // This works!
 });
-
-
